8680e8310b757a2fb4efed9af9111ca466260aa5
Jeff Robertson <jeff.robertson@gmail.com>
2023-03-07 18:45:24 -0500

    cleanup: comments, organization, removing unused code..

--- src/main/java/com/adaptershack/jeffdb/DatabaseService.java
+++ src/main/java/com/adaptershack/jeffdb/DatabaseService.java
@@ -28,12 +28,21 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 @Service
 public class DatabaseService {
 	
-	
+	/* The root directory where all files live.
+	 * When running in Spring Boot this is injected from the properties file.
+	 */
 	@Value("${com.adaptershack.jeffdb.root}")
 	String rootDirectory;
 	
+	/*
+	 * This inject's Spring's own single instance of ObjectMapper
+	 */
 	@Autowired
 	ObjectMapper objectMapper;
+	
+	/*
+	 * Getters and setters, for manual (non-injected) usage.
+	 */
 	
 	public String getRootDirectory() {
 		return rootDirectory;
@@ -51,28 +60,21 @@ public class DatabaseService {
 		this.objectMapper = objectMapper;
 	}
 
-	private void selfInit() {
-
-		// don't do this - make them initialize us
-		
-//		if(objectMapper == null) {
-//			objectMapper = new ObjectMapper();
-//		}
-//		
-//		if(rootDirectory==null) {
-//			rootDirectory="jeffdb";
-//		}
-		
-	}
 	
-	
-	private final static String ID = "id";
-	private final static String DOT_JSON = ".json";
 	
 
+	/**
+	 * Adds a new object to the specified collections.
+	 * If the object already contains a key called "id",
+	 * then the value of this field will be used to store
+	 * and retrieve the object. If no such field exists,
+	 * one will be randomly generated.
+	 * 
+	 * @param collection
+	 * @param row
+	 * @return
+	 */
 	public ObjectNode insert(String collection, ObjectNode row) {
-
-		selfInit();
 		
 		if(!row.has(ID)) {
 			row.put(ID, generateId());
@@ -96,11 +98,15 @@ public class DatabaseService {
 		
 	}
 
-	private String generateId() {
-		return UUID.randomUUID().toString().replaceAll("-", "");
-	}
 
-	
+	/**
+	 * Stores the object under the specified ID.
+	 * 
+	 * @param collection
+	 * @param id
+	 * @param row
+	 * @return
+	 */
 	public ObjectNode update(String collection, String id, ObjectNode row) {
 
 		row.put(ID, id);
@@ -108,8 +114,186 @@ public class DatabaseService {
 		return insert(collection,row);
 		
 	}
+
+	/**
+	 * Gets the object specified by the id, from the specified collection.
+	 * 
+	 * @param collection
+	 * @param id
+	 * @return
+	 */
+	public ObjectNode get(String collection, String id) {
+
+		File collectionDir = directoryExists(collection);
+		
+		File rowFile = new File( collectionDir, id + DOT_JSON );
+		
+		if(!rowFile.exists()) {
+			return null;
+		}
+		
+		try {
+			return (ObjectNode) objectMapper.readTree(rowFile);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
 	
+	/**
+	 * Deletes the object specified by the ID.
+	 * 
+	 * @param collection
+	 * @param id
+	 * @return
+	 */
+	public boolean delete(String collection, String id) {
+		
+		File collectionDir = directoryExists(collection);
+
+		File rowFile = new File( collectionDir, id + DOT_JSON );
+		
+		if(!rowFile.exists()) {
+			return false;
+		} else {
+			return rowFile.delete();
+		}
+		
+	}
+	
+	
+	/**
+	 * Simply gets all objects from the specified collection.
+	 * 
+	 * @param collection
+	 * @return
+	 */
+	public ArrayNode listAll(String collection) {
+		return list(collection,null);
+	}
+
+	
+	/**
+	 * Finds all objects in the collection where the predicate returns true.
+	 * 
+	 * Example:
+	 * 
+	 * JsonNode result =
+	 *    list(collection, (obj) -> obj.get("name").equalsIgnoreCase("jeff") );
+	 * 
+	 * 
+	 * @param collection
+	 * @param predicate
+	 * @return
+	 */
+	public ArrayNode list(String collection, Predicate<JsonNode> predicate ) {
+
+		File collectionDir = directoryExists(collection);
+		
+		ArrayNode list = objectMapper.createArrayNode();
+		
+		for( File rowFile : collectionDir.listFiles( (dir,name) -> name.endsWith(DOT_JSON))) {
+			try {
+				JsonNode row = objectMapper.readTree(rowFile);
+				if(predicate == null || predicate.test(row)) {
+					list.add(row);
+				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		
+		return list;
+	}
+
+
+	/**
+	 * Finds and DELETES all objects in the collection where the predicate returns true.
+	 * 
+	 * Example:
+	 * 
+	 *    deleteMatching(collection, (obj) -> obj.get("name").equalsIgnoreCase("jeff") );
+	 * 
+	 * 
+	 * @param collection
+	 * @param predicate
+	 * @return
+	 */
+	public boolean deleteMatching(String collection, Predicate<JsonNode> predicate ) {
+
+		JsonNode removed = list(collection, (row) ->
+			{ 
+				if(predicate.test(row)) {
+					delete(collection,row.get(ID).asText());
+					return true;
+				} else {
+					return false;
+				}
+			});
+		
+		return ! removed.isEmpty();
+	}
+	
+	/**
+	 * A convenience method for finding objects where the same keys,
+	 * having the same values, exist as in the provided object.
+	 * 
+	 * The query object need not have as many fields as the objects
+	 * in the collection. The most common use case is probably for it
+	 * to have only one field.
+	 * 
+	 * @param collection
+	 * @param params
+	 * @return
+	 */
+	public ArrayNode listMatching(String collection, JsonNode params) {
+		return list(collection, new MatchingP(params));
+	}
+	
+	/**
+	 * A convenience method for DELETING objects where the same keys,
+	 * having the same values, exist as in the provided object.
+	 * 
+	 * The query object need not have as many fields as the objects
+	 * in the collection. The most common use case is probably for it
+	 * to have only one field.
+	 * 
+	 * @param collection
+	 * @param params
+	 * @return
+	 */
+	public boolean deleteMatching(String collection, JsonNode params) {
+		return deleteMatching(collection, new MatchingP(params));
+	}
+	
+	/**
+	 * Deletes the entire collection and all of its objects.
+	 * 
+	 * @param collection
+	 * @return
+	 */
+	public boolean deleteCollection(String collection) {
+
+		checkRegex(collection);
+
+		File toDelete = new File(rootDirectory,collection);
+		
+		return FileSystemUtils.deleteRecursively(toDelete);
+		
+	}
+	
+	
+	
+	/*
+	 * "Magic" constants
+	 */
+	private final static String ID = "id";
+	private final static String DOT_JSON = ".json";
 	private final static String REGEX="^[A-Za-z0-9_.-]+";
+
+	private String generateId() {
+		return UUID.randomUUID().toString().replaceAll("-", "");
+	}
 	
 	private File directoryExists(String collection) {
 		
@@ -129,90 +313,9 @@ public class DatabaseService {
 		}
 	}
 
-	public ObjectNode get(String collection, String id) {
-
-		selfInit();
-
-		File collectionDir = directoryExists(collection);
-		
-		File rowFile = new File( collectionDir, id + DOT_JSON );
-		
-		if(!rowFile.exists()) {
-			return null;
-		}
-		
-		try {
-			return (ObjectNode) objectMapper.readTree(rowFile);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		
-	}
-	
-	public ArrayNode listAll(String collection) {
-		return list(collection,null);
-	}
-
-	public ArrayNode listMatching(String collection,String key, String value) {
-		return list(collection,
-			(row) -> row.has(key) && row.get(key).asText().equals(value)
-		);
-	}
-	
-	
-	public ArrayNode list(String collection, Predicate<JsonNode> predicate ) {
-
-		selfInit();
-
-		File collectionDir = directoryExists(collection);
-		
-		ArrayNode list = objectMapper.createArrayNode();
-		
-		for( File rowFile : collectionDir.listFiles( (dir,name) -> name.endsWith(DOT_JSON))) {
-			try {
-				JsonNode row = objectMapper.readTree(rowFile);
-				if(predicate == null || predicate.test(row)) {
-					list.add(row);
-				}
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		}
-		
-		return list;
-	}
-	
-	public boolean delete(String collection, String id) {
-		
-		File collectionDir = directoryExists(collection);
-
-		File rowFile = new File( collectionDir, id + DOT_JSON );
-		
-		if(!rowFile.exists()) {
-			return false;
-		} else {
-			return rowFile.delete();
-		}
-		
-	}
-
-	public boolean deleteMatching(String collection, Predicate<JsonNode> predicate ) {
-
-		JsonNode removed = list(collection, (row) ->
-			{ 
-				if(predicate.test(row)) {
-					delete(collection,row.get(ID).asText());
-					return true;
-				} else {
-					return false;
-				}
-			});
-		
-		return ! removed.isEmpty();
-	}
 
 	
-	static class MatchingP implements Predicate<JsonNode> {
+	private static class MatchingP implements Predicate<JsonNode> {
 
 		JsonNode params;
 		
@@ -240,22 +343,5 @@ public class DatabaseService {
 	}
 	
 	
-	public ArrayNode listMatching(String collection, JsonNode params) {
-		return list(collection, new MatchingP(params));
-	}
-	
-	public boolean deleteMatching(String collection, JsonNode params) {
-		return deleteMatching(collection, new MatchingP(params));
-	}
-	
-	public boolean deleteCollection(String collection) {
-
-		checkRegex(collection);
-
-		File toDelete = new File(rootDirectory,collection);
-		
-		return FileSystemUtils.deleteRecursively(toDelete);
-		
-	}
 	
 }
